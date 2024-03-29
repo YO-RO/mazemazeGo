813fdcfbb3a07adff7a4180dec12f56ee66ea008
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-03-30 02:16:39 +0900

    feat: プレイヤーを動かせるようにする

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/mattn/go-tty"
+)
 
 type MapDetailType [10][10]int
 
@@ -23,6 +28,11 @@ func (p Position) isAt(x, y int) bool {
 	return p.X == x && p.Y == y
 }
 
+func (p *Position) move(pos Position) {
+	p.X += pos.X
+	p.Y += pos.Y
+}
+
 func makeStageStr(mazeMap MapDetailType, player Position) string {
 	var mazeMapStr string
 	for y, rows := range mazeMap {
@@ -41,7 +51,41 @@ func makeStageStr(mazeMap MapDetailType, player Position) string {
 	return mazeMapStr
 }
 
+func isValidDirectionPos(directionPos Position) bool {
+	return directionPos.isSame(Position{1, 0}) ||
+		directionPos.isSame(Position{-1, 0}) ||
+		directionPos.isSame(Position{0, 1}) ||
+		directionPos.isSame(Position{0, -1})
+}
+
+func movePlayer(mazeMap MapDetailType, player Position, directionPos Position) (Position, bool) {
+	if !(isValidDirectionPos(directionPos)) {
+		return Position{}, false
+	}
+
+	player.move(directionPos)
+	x, y := player.X, player.Y
+
+	minX, minY := 0, 0
+	maxX, maxY := len(mazeMap[0])-1, len(mazeMap)-1
+	if player.X < minX || player.X > maxX ||
+		player.Y < minY || player.Y > maxY {
+		return Position{}, false
+	}
+
+	if mazeMap[y][x] == 0 {
+		return player, true
+	}
+	return Position{}, false
+}
+
 func main() {
+	tty, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tty.Close()
+
 	maze := Maze{
 		MapDetail: MapDetailType{
 			{1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -59,6 +103,33 @@ func main() {
 		ExitPos:     Position{6, 9},
 	}
 	player := maze.EntrancePos
-	stageStr := makeStageStr(maze.MapDetail, player)
-	fmt.Print(stageStr)
+
+	for {
+		stageStr := makeStageStr(maze.MapDetail, player)
+		fmt.Println(stageStr)
+
+		r, err := tty.ReadRune()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if r == 'x' {
+			break
+		}
+
+		var dirPos Position
+		switch r {
+		case 'w':
+			dirPos = Position{0, -1}
+		case 's':
+			dirPos = Position{0, 1}
+		case 'a':
+			dirPos = Position{-1, 0}
+		case 'd':
+			dirPos = Position{1, 0}
+		}
+		if p, ok := movePlayer(maze.MapDetail, player, dirPos); ok {
+			player = p
+		}
+	}
 }
