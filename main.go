058f2eee592bd57cbfc97156e5683240e778d000
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-04-28 02:43:20 +0900

    refactor: ゲームに関するコアな処理をmain.goから適したファイルに移動する
    mazemazego.go: ゲームのロジックに関するコード。
    maze_design.go: 迷路の設計に関するコード。将来的に迷路の自動生成を考えているため、このファイルに分けた。

--- main.go
+++ main.go
@@ -3,67 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	"mazemazego/position"
 
 	"github.com/mattn/go-tty"
 )
-
-type MapDesignType [10][10]int
-
-type MazeDesign struct {
-	Design      MapDesignType
-	EntrancePos position.Pos
-	ExitPos     position.Pos
-}
-
-func makeStageStr(design MapDesignType, playerPos position.Pos) string {
-	var mazeMapStr string
-	for y, rows := range design {
-		for x, mass := range rows {
-			switch {
-			case playerPos.X == x && playerPos.Y == y:
-				mazeMapStr += "OO"
-			case mass == 1:
-				mazeMapStr += "##"
-			case mass == 0:
-				mazeMapStr += "  "
-			}
-		}
-		mazeMapStr += "\n"
-	}
-	return mazeMapStr
-}
-
-func isValidDirectionPos(directionPos position.Pos) bool {
-	return position.IsSame(directionPos, position.Pos{X: 1, Y: 0}) ||
-		position.IsSame(directionPos, position.Pos{X: -1, Y: 0}) ||
-		position.IsSame(directionPos, position.Pos{X: 0, Y: 1}) ||
-		position.IsSame(directionPos, position.Pos{X: 0, Y: -1})
-}
-
-func movePlayer(playerPos position.Pos, directionPos position.Pos, mazeMap MapDesignType) (position.Pos, bool) {
-	if !(isValidDirectionPos(directionPos)) {
-		return position.Pos{}, false
-	}
-
-	newPlayerPos := position.Add(playerPos, directionPos)
-
-	minX, minY := 0, 0
-	maxX, maxY := len(mazeMap[0])-1, len(mazeMap)-1
-	if newPlayerPos.X < minX || newPlayerPos.X > maxX ||
-		newPlayerPos.Y < minY || newPlayerPos.Y > maxY {
-		return position.Pos{}, false
-	}
-
-	if mazeMap[newPlayerPos.Y][newPlayerPos.X] == 0 {
-		return newPlayerPos, true
-	}
-	return position.Pos{}, false
-}
-
-func escaped(playerPos, exitPos position.Pos) bool {
-	return position.IsSame(playerPos, exitPos)
-}
 
 func main() {
 	tty, err := tty.Open()
@@ -72,30 +14,12 @@ func main() {
 	}
 	defer tty.Close()
 
-	maze := MazeDesign{
-		Design: MapDesignType{
-			{1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-			{1, 0, 1, 0, 1, 0, 0, 0, 1, 1},
-			{1, 0, 1, 0, 1, 0, 1, 0, 1, 1},
-			{1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
-			{1, 0, 0, 1, 1, 1, 1, 0, 1, 1},
-			{1, 0, 1, 1, 0, 0, 0, 0, 0, 1},
-			{1, 0, 1, 0, 0, 1, 1, 1, 0, 1},
-			{1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
-			{1, 0, 0, 1, 1, 1, 0, 1, 0, 1},
-			{1, 1, 1, 1, 1, 1, 0, 1, 1, 1},
-		},
-		EntrancePos: position.Pos{X: 1, Y: 0},
-		ExitPos:     position.Pos{X: 6, Y: 9},
-	}
-	playerPos := maze.EntrancePos
-
+	mmg := NewMazeMazeGo(GetMazeDesign())
 GAME_LOOP:
 	for {
-		stageStr := makeStageStr(maze.Design, playerPos)
-		fmt.Println(stageStr)
+		fmt.Println(mmg)
 
-		if escaped(playerPos, maze.ExitPos) {
+		if mmg.IsEscaped() {
 			fmt.Println("Successful Escape!")
 			return
 		}
@@ -110,25 +34,13 @@ GAME_LOOP:
 		case 'x':
 			break GAME_LOOP
 		case 'w':
-			up := position.Pos{X: 0, Y: -1}
-			if p, ok := movePlayer(playerPos, up, maze.Design); ok {
-				playerPos = p
-			}
+			mmg.MovePlayer(Up)
 		case 's':
-			down := position.Pos{X: 0, Y: 1}
-			if p, ok := movePlayer(playerPos, down, maze.Design); ok {
-				playerPos = p
-			}
+			mmg.MovePlayer(Down)
 		case 'a':
-			left := position.Pos{X: -1, Y: 0}
-			if p, ok := movePlayer(playerPos, left, maze.Design); ok {
-				playerPos = p
-			}
+			mmg.MovePlayer(Left)
 		case 'd':
-			right := position.Pos{X: 1, Y: 0}
-			if p, ok := movePlayer(playerPos, right, maze.Design); ok {
-				playerPos = p
-			}
+			mmg.MovePlayer(Right)
 		}
 	}
 }
--- position.go
+++ position.go
@@ -1,14 +1,14 @@
-package position
+package main
 
 type Pos struct {
 	X int
 	Y int
 }
 
-func IsSame(p1, p2 Pos) bool {
+func IsSamePos(p1, p2 Pos) bool {
 	return p1.X == p2.X && p1.Y == p2.Y
 }
 
-func Add(p1, p2 Pos) Pos {
+func AddPos(p1, p2 Pos) Pos {
 	return Pos{X: p1.X + p2.X, Y: p1.Y + p2.Y}
 }
