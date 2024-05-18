2846c79ed2f95610fd63e51d5ba2aa67b5855634
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-05-19 03:19:34 +0900

    feat: ゲームを常にターミナルの同じ位置に表示するようにする

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/mattn/go-tty"
 )
@@ -15,9 +16,19 @@ func main() {
 	defer tty.Close()
 
 	mmg := NewMazeMazeGo(GetMaze())
+	printedLine := 0
 GAME_LOOP:
 	for {
-		fmt.Println(mmg)
+		// 前回の表示内容を消去してからmmgを表示する
+		if printedLine > 0 {
+			// 表示内容を消去する
+			// \033[nF -> カーソルをn行上の先頭に移動
+			//	(nが0の時、1行上の先頭に移動してしまう)
+			// \033[0J -> カーソルより後ろの画面をクリア
+			fmt.Printf("\033[%dF\033[0J", printedLine)
+		}
+		fmt.Print(mmg)
+		printedLine = strings.Count(mmg.String(), "\n")
 
 		if mmg.GameIsEnd() {
 			return
